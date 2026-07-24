378827fe49d042591a6f7fdc554e5923089d8c84
Ayush Singh <ayush5292singh@gmail.com>
2026-07-24 21:12:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,138 @@ document.getElementById("answer").innerHTML =
 "🤖 I will help you learn about: " + question;
 
 }
+// =====================
+// TO DO LIST
+// =====================
+
+
+let tasks=[];
+
+
+function addTask(){
+
+let task =
+document.getElementById("task").value;
+
+
+if(task===""){
+
+alert("Enter task");
+
+return;
+
+}
+
+
+tasks.push(task);
+
+
+showTasks();
+
+
+document.getElementById("task").value="";
+
+}
+
+
+
+function showTasks(){
+
+let result="";
+
+
+tasks.forEach(function(t,index){
+
+
+result +=
+
+"<p>📌 "+t+
+" <button onclick='deleteTask("+index+")'>❌</button></p>";
+
+
+});
+
+
+document.getElementById("taskList").innerHTML=result;
+
+
+}
+
+
+
+function deleteTask(index){
+
+tasks.splice(index,1);
+
+showTasks();
+
+}
+
+
+
+// =====================
+// TIMETABLE
+// =====================
+
+
+let schedule=[];
+
+
+function addSchedule(){
+
+
+let subject =
+document.getElementById("subject").value;
+
+
+let time =
+document.getElementById("time").value;
+
+
+
+if(subject==="" || time===""){
+
+alert("Enter subject and time");
+
+return;
+
+}
+
+
+schedule.push({
+
+subject:subject,
+time:time
+
+});
+
+
+showSchedule();
+
+
+}
+
+
+
+function showSchedule(){
+
+
+let result="";
+
+
+schedule.forEach(function(item){
+
+
+result +=
+
+"<p>📚 "+item.subject+
+" ⏰ "+item.time+"</p>";
+
+
+});
+
+
+document.getElementById("scheduleList").innerHTML=result;
+
+
+}
